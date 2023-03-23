591af0327efa58fa7e26828d8ff839dedb8708d3
bootcamparankush <parankush.somraj@gmail.com>
2023-03-23 16:00:47 -0400

    added sql statements for schema and table creation

--- emptracker.js
+++ emptracker.js
@@ -1,5 +1,5 @@
-// const inquirer = require("inquirer");
-// const mysql = require("mysql");
+const inquirer = require("inquirer");
+const mysql = require("mysql");
 // const consoleTable = require("console.table");
 
 const connection = mysql.createConnection({
